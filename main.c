30bb51e675f60e3f3fbe2bd9a1a696c9c62bf7a1
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2018-10-26 10:01:56 -0400

    Começando as cartas especiais
    Olha la tem gente que tá cagando mesmo pro que ta rolando

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 
 int main(){
   int carta_atual = 0;
+  
+  /*
+  "5B" // 5 Blue
+  "+G" // +2 Green
+  "JW" // Joker
+  "IY" // Inversao Yellow
+  "C4" // Joker 4 Cartas
+  "PR" // Pulante (Bloqueio, perde a vez) Red
+  */
 
   struct Carta{
     char valor;
@@ -11,18 +20,12 @@ int main(){
   };
 
   char cores[4] = {'R', 'G', 'B', 'Y'};
+  char especiais[3] = {'I', 'P', '+'};
 
   struct Carta baralho[108];
 
 
-/*
-"5B" // 5 Blue
-"+G" // +2 Green
-"JW" // Joker
-"IY" // Inversao Yellow
-"C4" // Joker 4 Cartas
-"PR" // Pulante (Bloqueio, perde a vez) Red
-*/
+
 
 // Aqui rola uma geração de deck com todas as cartas
   int cartaAtual = 0;
@@ -44,13 +47,31 @@ int main(){
     }
   }
 
+  // Criar cartas especiais
+  for(i=0; i<=3; i++)
+  {
+    for(j=0;j<4; j++)
+      {
+        struct Carta nova = {especiais[i], cores[j]};
+
+        baralho[cartaAtual] = nova;
+        cartaAtual++;
+
+        baralho[cartaAtual] = nova;
+        cartaAtual++; 
+
+      }
+  }
+  
   // vamos ver se até aqui ta tudo razoável
   for(i=0;i<cartaAtual;i++)
   {
     printf("%c %c\n",baralho[i].valor, baralho[i].cor);
   }
-}
   
+} // FIM DO MAIN
+ 
+
   
   
 
