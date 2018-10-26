76df8892b6f4865a3d4edc2bdb7ae90d9256839b
Rodrigo Nishino <rodrigo.nsh@gmail.com>
2018-10-26 08:25:02 -0400

    Olha lá
    Alpha 0000 Prototipo da ideia do projeto da intenção da vontade do ser vivente

---
+++ main.c
@@ -0,0 +1,22 @@
+// Este é o código do sevidor
+
+#include <stdio.h>
+
+char deck[108];
+
+/*
+"5B" // 5 Blue
+"+G" // +2 Green
+"J" // Joker Red
+"IY" // Inversao Yellow
+"J4" // Joker 4 Cartas
+"PR" // Pulante (Bloqueio, perde a vez) Red
+*/
+
+// Aqui rola uma geração de deck com todas as cartas
+
+// Aqui rola um shuffle https://stackoverflow.com/questions/6127503/shuffle-array-in-c
+
+// Tem quer os player
+
+// TODO: ter um sevidor aqui https://pt.wikibooks.org/wiki/Programar_em_C/Sockets
