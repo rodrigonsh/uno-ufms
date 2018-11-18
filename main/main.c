75e03da5d7385912da4f1a7abebc4a8025630779
Drawiin <37352066+Drawiin@users.noreply.github.com>
2018-11-18 16:26:42 -0300

    Refatoração do código
    Adequando o código a padrões mais adequados e seguros, de acordo com a revisão ISO/IEC 9899:2011 da linguagem C.

--- main/main.c
+++ main/main.c
@@ -1,33 +1,7 @@
-#include <stdio.h>
-#include<stdlib.h>
-
-const int size = 108;
-
-  struct Carta{
-    char valor;
-    char cor;
-  };
-
-
-void shuffle(struct Carta array[], size_t n)
-{
-  size_t i;
-  if (n > 1) 
-  {
-    for (i = 0; i <= n - 1; i++) 
-    {
-      size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
-      struct Carta t = array[j];
-      array[j] = array[i];
-      array[i] = t;
-    }
-  }
-}
-
-int main(){
-  int carta_atual = 0;
-  
-  /*
+#include "uno_ser.h"
+#define size 108
+ /*
+ Exemplos
   "5B" // 5 Blue
   "+G" // +2 Green
   "JW" // Joker
@@ -36,71 +10,17 @@ int main(){
   "PR" // Pulante (Bloqueio, perde a vez) Red
    "uno"// aviso de uma carta
    */
+/*Compilar usando
+gcc -o uno main.c uno_ser.c*/
 
-
-  char cores[4] = {'R', 'G', 'B', 'Y'};
-  char especiais[3] = {'I', 'P', '+'};
-  int uno=0;
+int main(int argc, char const *argv[])
+{
   struct Carta baralho[size];
-
-// Aqui rola uma geração de deck com todas as cartas
-  int cartaAtual = 0;
-  int i,j;
-  for(i=0; i<= 9; i++)
-  {
-
-    for(j=0;j<4; j++)
-    {
-      struct Carta nova = {i+'0', cores[j]};
-      baralho[cartaAtual] = nova;
-      cartaAtual++;
-
-      if (i > 0)
-      {
-        baralho[cartaAtual] = nova;
-        cartaAtual++;
-      }
-    }
-  }
-
-  // Criar cartas especiais
-  for(i=0; i<3; i++)
-  {
-    for(j=0;j<4; j++) 
-    {
-        struct Carta nova = {especiais[i], cores[j]};
-
-        baralho[cartaAtual] = nova;
-        cartaAtual++;
-
-        baralho[cartaAtual] = nova;
-        cartaAtual++; 
-
-      }
-  }
-  // Criar coringa
- for(j=0;j<4; j++) 
- {
-    struct Carta coringa4 = {'C', '4'};
-    struct Carta joker = {'J', 'W'};
-   
-    baralho[cartaAtual] = coringa4;
-    cartaAtual++;
-   
-    baralho[cartaAtual] = joker;
-    cartaAtual++;
-   
-  }
-  
-  shuffle(baralho, 108);
-  
-  // vamos ver se até aqui ta tudo razoável
-  for(i=0;i<cartaAtual;i++)
-  {
-    printf("%c %c\n",baralho[i].valor, baralho[i].cor);
-  }
-  printf("%d\n", cartaAtual);
+  //Gera as cartas
+  gera_deck(baralho, size);
+  //Embaralha as cartas
+  shuffle(baralho, size);
+  //Mostra todas as cartas
+  mostra_deck(baralho, size);
   return 0;
-} 
-
-
+}
