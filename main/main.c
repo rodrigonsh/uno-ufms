cb4421cba1d5e3b7663bb6702ba2bca0032238ac
Drawiin <37352066+Drawiin@users.noreply.github.com>
2018-12-08 18:59:00 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,41 +1,21 @@
 #include "uno.h"
 #define size 108
-
-int topo;
-/*
-Exemplos
-"5B" // 5 Blue
-"+G" // +2 Green
-"JW" // Joker
-"IY" // Inversao Yellow
-"C4" // Joker 4 Cartas
-"PR" // Pulante (Bloqueio, perde a vez) Red
-"uno"// aviso de uma carta
-
-Compilar usando > > > gcc -o uno main.c uno.c
-*/
+//Compilar usandoo caomando >>> gcc -o uno main.c uno.c
+int n_jogadores;
+struct Pilha baralho, jogo;
+struct Jogador jogadores[10];
 
 int main(int argc, char const *argv[])
 {
-	int n_jogadores, i;
-	struct Carta baralho[size];
-	struct Jogador jogadores[10];
-	//Gera as cartas
-	gera_deck(baralho, size);
-	//Embaralha as cartas
-	shuffle(baralho, size);
-	//distribuir cartas
-	printf("Quantos jogadores ?");
-	scanf("%d", &n_jogadores);
-  	topo = size - 1;
-	topo = distribui_cartas(baralho, topo, jogadores, n_jogadores);
-	for(i = 0; i < n_jogadores; i++)
+	int i;
+	n_jogadores = 2;
+	gera_cartas(&baralho);
+	embaralha(&baralho);
+	distribui_cartas(&baralho ,jogadores, n_jogadores);
+	for (i = 0; i < n_jogadores; i++)
 	{
-		printf("\n\nJogador n %d\n", jogadores[i].ind_jogador);
-		printf("============================================\n");
-		mostra_deck(jogadores[i].mao, jogadores[i].n_cartas);
+		printf("\njogador %d\n", i + 1);
+		mostra_cartas(jogadores[i].mao, jogadores[i].n_cartas);
 	}
-	printf("\n==================BARALHO==================\n");
-	mostra_deck(baralho, topo);
 	return 0;
 }
